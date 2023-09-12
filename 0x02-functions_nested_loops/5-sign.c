77b3b910f319909a78dad131baf57a5a3b57353e
AhmedAlbashaDev <ahmedmohammedkhier@gmail.com>
2023-09-12 05:41:21 -0700

    m

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,13 +2,13 @@
 
 /**
  * print_sign - print + if n is greater than zero,
- * 		0 if n is zero and - if n is less
- * 		than zero
+ *	0 if n is zero and - if n is less
+ *	than zero
  *
  * @c: take integer type input for function.
  *
  * Return: 1 if +, 0 if 0 and -1 if -
- */
+*/
 
 int print_sign(int n)
 {
